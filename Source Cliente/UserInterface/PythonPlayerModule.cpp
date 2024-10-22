3515d713f981534d3e13f5fa8e1c240219a2d2b4
Nazox <dani6359@hotmail.es>
2024-10-22 16:54:09 +0200

    Update PythonPlayerModule.cpp

--- Source Cliente/UserInterface/PythonPlayerModule.cpp
+++ Source Cliente/UserInterface/PythonPlayerModule.cpp
@@ -1,69 +1,138 @@
 //Find this function
-PyObject * chatGetLinkFromHyperlink(PyObject * poSelf, PyObject * poArgs)
+PyObject * playerGetItemLink(PyObject * poSelf, PyObject * poArgs)
 {
   [...]
 }
 
 //And replace all with this
-PyObject * chatGetLinkFromHyperlink(PyObject * poSelf, PyObject * poArgs)
+#ifdef ENABLE_MORE_SOCKETS
+PyObject * playerGetItemLink(PyObject * poSelf, PyObject * poArgs)
 {
-    char * szHyperlink;
-    
-    if (!PyTuple_GetString(poArgs, 0, &szHyperlink))
-        return Py_BuildException();
+    TItemPos Cell;
 
-    std::string stHyperlink(szHyperlink);
-    std::vector<std::string> results;
-
-    split_string(stHyperlink, ":", results, false);
-
-    // item:vnum:flag:socket0:socket1:socket2:socket3:socket4:socket5
-    if (0 == results[0].compare("item"))
+    switch (PyTuple_Size(poArgs))
     {
-        if (results.size() < 9)
-            return Py_BuildValue("s", "");
+    case 1:    
+        if (!PyTuple_GetInteger(poArgs, 0, &Cell.cell))
+            return Py_BuildException();
+        break;
+    case 2:
+        if (!PyTuple_GetByte(poArgs, 0, &Cell.window_type))
+            return Py_BuildException();
+        if (!PyTuple_GetInteger(poArgs, 1, &Cell.cell))
+            return Py_BuildException();
+        break;
+    default:
+        return Py_BuildException();
+    }
+    const TItemData * pPlayerItem = CPythonPlayer::Instance().GetItemData(Cell);
+    CItemData * pItemData = NULL;
+    char buf[1024];
 
-        CItemData * pItemData = NULL;
+    if (pPlayerItem && CItemManager::Instance().GetItemDataPointer(pPlayerItem->vnum, &pItemData))
+    {
+        char itemlink[256];
+        int len;
+        bool isAttr = false;
 
-        if (CItemManager::Instance().GetItemDataPointer(htoi(results[1].c_str()), &pItemData))
-        {
-            char buf[1024] = { 0 };
-            char itemlink[256];
-            int len;
-            bool isAttr = false;
+        len = snprintf(itemlink, sizeof(itemlink), "item:%x:%x:%x:%x:%x:%x:%x:%x", //6 Piedras
+                pPlayerItem->vnum, pPlayerItem->flags,
+                pPlayerItem->alSockets[0], //1 Piedras
+                pPlayerItem->alSockets[1], //2 Piedras
+                pPlayerItem->alSockets[2], //3 Piedras
+                pPlayerItem->alSockets[3], //4 Piedras
+                pPlayerItem->alSockets[4], //5 Piedras
+                pPlayerItem->alSockets[5]); //6 Piedras
 
-            len = snprintf(itemlink, sizeof(itemlink), "item:%x:%x:%x:%x:%x:%x:%x:%x", 
-                    htoi(results[1].c_str()),
-                    htoi(results[2].c_str()),
-                    htoi(results[3].c_str()),
-                    htoi(results[4].c_str()),
-                    htoi(results[5].c_str()),
-                    htoi(results[6].c_str()),
-                    htoi(results[7].c_str()),
-                    htoi(results[8].c_str()));
-
-            if (results.size() >= 11)
+        for (int i = 0; i < ITEM_ATTRIBUTE_SLOT_MAX_NUM; ++i)
+            if (pPlayerItem->aAttr.bType != 0)
             {
-                for (int i = 9; i < results.size(); i += 2)
-                {
-                    len += snprintf(itemlink + len, sizeof(itemlink) - len, ":%x:%d", 
-                            htoi(results.c_str()),
-                            atoi(results[i+1].c_str()));
-                    isAttr = true;
-                }
+                len += snprintf(itemlink + len, sizeof(itemlink) - len, ":%x:%d", 
+                        pPlayerItem->aAttr.bType, pPlayerItem->aAttr.sValue);
+                isAttr = true;
             }
 
+        if( GetDefaultCodePage() == CP_ARABIC ) {
+            if (isAttr)
+                //"item:¹øÈ£:ÇÃ·¡±×:¼ÒÄÏ0:¼ÒÄÏ1:¼ÒÄÏ2"
+                snprintf(buf, sizeof(buf), " |h|r[%s]|cffffc700|H%s|h", pItemData->GetName(), itemlink);
+            else
+                snprintf(buf, sizeof(buf), " |h|r[%s]|cfff1e6c0|H%s|h", pItemData->GetName(), itemlink);
+        } else {
             if (isAttr)
                 //"item:¹øÈ£:ÇÃ·¡±×:¼ÒÄÏ0:¼ÒÄÏ1:¼ÒÄÏ2"
                 snprintf(buf, sizeof(buf), "|cffffc700|H%s|h[%s]|h|r", itemlink, pItemData->GetName());
             else
                 snprintf(buf, sizeof(buf), "|cfff1e6c0|H%s|h[%s]|h|r", itemlink, pItemData->GetName());
-
-            return Py_BuildValue("s", buf);
         }
     }
+    else
+        buf[0] = '0';
 
-    return Py_BuildValue("s", "");
+    return Py_BuildValue("s", buf);
 }
+#else
+PyObject * playerGetItemLink(PyObject * poSelf, PyObject * poArgs)
+{
+	TItemPos Cell;
+
+	switch (PyTuple_Size(poArgs))
+	{
+	case 1:	
+		if (!PyTuple_GetInteger(poArgs, 0, &Cell.cell))
+			return Py_BuildException();
+		break;
+	case 2:
+		if (!PyTuple_GetByte(poArgs, 0, &Cell.window_type))
+			return Py_BuildException();
+		if (!PyTuple_GetInteger(poArgs, 1, &Cell.cell))
+			return Py_BuildException();
+		break;
+	default:
+		return Py_BuildException();
+	}
+	const TItemData * pPlayerItem = CPythonPlayer::Instance().GetItemData(Cell);
+	CItemData * pItemData = NULL;
+	char buf[1024];
+
+	if (pPlayerItem && CItemManager::Instance().GetItemDataPointer(pPlayerItem->vnum, &pItemData))
+	{
+		char itemlink[256];
+		int len;
+		bool isAttr = false;
+
+		len = snprintf(itemlink, sizeof(itemlink), "item:%x:%x:%x:%x:%x", 
+				pPlayerItem->vnum, pPlayerItem->flags,
+				pPlayerItem->alSockets[0], pPlayerItem->alSockets[1], pPlayerItem->alSockets[2]);
+
+		for (int i = 0; i < ITEM_ATTRIBUTE_SLOT_MAX_NUM; ++i)
+			if (pPlayerItem->aAttr[i].bType != 0)
+			{
+				len += snprintf(itemlink + len, sizeof(itemlink) - len, ":%x:%d", 
+						pPlayerItem->aAttr[i].bType, pPlayerItem->aAttr[i].sValue);
+				isAttr = true;
+			}
+    
+		if (GetDefaultCodePage() == CP_ARABIC) {
+			if (isAttr)
+				//
+				snprintf(buf, sizeof(buf), " |h|r[%s]|cffffc700|H%s|h", pItemData->GetName(), itemlink);
+			else
+				snprintf(buf, sizeof(buf), " |h|r[%s]|cfff1e6c0|H%s|h", pItemData->GetName(), itemlink);
+		}
+		else {
+			if (isAttr)
+				//
+				snprintf(buf, sizeof(buf), "|cffffc700|H%s|h[%s]|h|r", itemlink, pItemData->GetName());
+			else
+				snprintf(buf, sizeof(buf), "|cfff1e6c0|H%s|h[%s]|h|r", itemlink, pItemData->GetName());
+		}
+	}
+	else
+		buf[0] = '\0';
+
+	return Py_BuildValue("s", buf);
+}
+#endif
 
 //If you don't want replace, increse all values with new sockets 
